c73841adc7ebee33b6d50374b1da5a3de9953f35
Karel Babka <krlbabka@gmail.com>
2024-07-13 20:26:26 +0200

    Updated movement, added coyote jump & double jump

--- Assets/Hra/Scripts/GameScene/Player/CharacterController2D.cs
+++ Assets/Hra/Scripts/GameScene/Player/CharacterController2D.cs
@@ -3,135 +3,109 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
-	[SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
-	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
-	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
-	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
-	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
-	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
-	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
-	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
+	[SerializeField] private float m_JumpForce = 400f;
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
+	[SerializeField] private LayerMask m_WhatIsGround;
+	[SerializeField] private Transform m_GroundCheck;
 
 	const float k_GroundedRadius = .55f; // Radius of the overlap circle to determine if grounded
-	private bool m_Grounded;            // Whether or not the player is grounded.
-	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
-	private Rigidbody2D m_Rigidbody2D;
+	public static bool m_Grounded { get; private set; }
+    private Rigidbody2D m_Rigidbody2D;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
 
-	[Header("Events")]
-	[Space]
+	[Header("TOGGLE ABILITIES")]
+	public bool canDash = false;
+    public bool canCoyoteJump = false;
+    public bool canDoubleJump = false;
 
-	public UnityEvent OnLandEvent;
 
-	[System.Serializable]
-	public class BoolEvent : UnityEvent<bool> { }
 
-	public BoolEvent OnCrouchEvent;
-	private bool m_wasCrouching = false;
+    private float _time;
+	private bool wantsToJump;
 
-	private void Awake()
+    private void Awake()
 	{
 		m_Rigidbody2D = GetComponent<Rigidbody2D>();
-
-		if (OnLandEvent == null)
-			OnLandEvent = new UnityEvent();
-
-		if (OnCrouchEvent == null)
-			OnCrouchEvent = new BoolEvent();
 	}
 
-	private void FixedUpdate()
+	private float _leftGroundAt;
+    private float coyoteJumpOffset = 0.1f;
+	private bool doubleJumpCharge;
+
+    private void FixedUpdate()
 	{
 		bool wasGrounded = m_Grounded;
 		m_Grounded = false;
-
-		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
 		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
 		for (int i = 0; i < colliders.Length; i++)
 		{
 			if (colliders[i].gameObject != gameObject)
 			{
 				m_Grounded = true;
-				if (!wasGrounded)
-					OnLandEvent.Invoke();
+				doubleJumpCharge = true;
 			}
+		}
+		if (wasGrounded && !m_Grounded)
+		{
+			_leftGroundAt = _time;
 		}
 	}
 
-
-	public void Move(float move, bool crouch, bool jump)
+	public void JumpPressed()
 	{
-		// If crouching, check to see if the character can stand up
-		if (!crouch)
-		{
-			// If the character has a ceiling preventing them from standing up, keep them crouching
-			if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
-			{
-				crouch = true;
-			}
-		}
-
-		//only control the player if grounded or airControl is turned on
-		if (m_Grounded || m_AirControl)
-		{
-
-			// If crouching
-			if (crouch)
-			{
-				if (!m_wasCrouching)
-				{
-					m_wasCrouching = true;
-					OnCrouchEvent.Invoke(true);
-				}
-
-				// Reduce the speed by the crouchSpeed multiplier
-				move *= m_CrouchSpeed;
-
-				// Disable one of the colliders when crouching
-				if (m_CrouchDisableCollider != null)
-					m_CrouchDisableCollider.enabled = false;
-			} else
-			{
-				// Enable the collider when not crouching
-				if (m_CrouchDisableCollider != null)
-					m_CrouchDisableCollider.enabled = true;
-
-				if (m_wasCrouching)
-				{
-					m_wasCrouching = false;
-					OnCrouchEvent.Invoke(false);
-				}
-			}
-
-			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-			// And then smoothing it out and applying it to the character
-			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-
-			// If the input is moving the player right and the player is facing left...
-			if (move > 0 && !m_FacingRight)
-			{
-				// ... flip the player.
-				Flip();
-			}
-			// Otherwise if the input is moving the player left and the player is facing right...
-			else if (move < 0 && m_FacingRight)
-			{
-				// ... flip the player.
-				Flip();
-			}
-		}
-		// If the player should jump...
-		if (m_Grounded && jump)
-		{
-			// Add a vertical force to the player.
-			m_Grounded = false;
-			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-		}
+		wantsToJump = true;
 	}
 
+    private void Update()
+    {
+        _time += Time.deltaTime;
+    }
+
+    private bool coyoteJumpPossible => canCoyoteJump && !m_Grounded && _time < _leftGroundAt + coyoteJumpOffset;
+    private bool doubleJumpPossible => canDoubleJump && !m_Grounded && doubleJumpCharge;
+
+    public void Move(float move)
+	{
+		// Move the character by finding the target velocity
+		Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+		// And then smoothing it out and applying it to the character
+		m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+		if ((move > 0 && !m_FacingRight) || (move < 0 && m_FacingRight)) 
+		{
+			Flip();
+		}
+
+		if (wantsToJump)
+		{
+            if (m_Grounded)
+            {
+				m_Grounded = false;
+				m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+            }
+			else
+			{
+				if (coyoteJumpPossible)
+				{
+					m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+				}
+                if (doubleJumpPossible)
+                {
+                    //m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+                    // Reset the vertical velocity
+                    Vector2 currentVelocity = m_Rigidbody2D.velocity;
+                    currentVelocity.y = 0;
+                    m_Rigidbody2D.velocity = currentVelocity;
+
+                    // Apply the jump force
+                    m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+                    doubleJumpCharge = false;
+                }
+            }
+        }
+		wantsToJump = false;
+	}
 
 	private void Flip()
 	{
--- Assets/Hra/Scripts/GameScene/Player/PlayerMovement.cs
+++ Assets/Hra/Scripts/GameScene/Player/PlayerMovement.cs
@@ -7,17 +7,21 @@ public class PlayerMovement : MonoBehaviour
     public float horizontalMultiplier = 40f;
     private float horizontalMove = 0f;
     private bool jump = false;
+    private float _time;
 
     void Update()
     {
+        _time += Time.deltaTime;
         horizontalMove = Input.GetAxisRaw("Horizontal") * horizontalMultiplier;
 
-        if (Input.GetButtonDown("Jump")) jump = true;
+        if (Input.GetButtonDown("Jump")) 
+        {
+            controller.JumpPressed();
+        }
     }
 
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        jump = false;
+        controller.Move(horizontalMove * Time.fixedDeltaTime);
     }
 }
